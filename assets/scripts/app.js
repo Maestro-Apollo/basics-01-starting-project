fa9eeefd8727e4102c82640c6370a4dd469ab554
Rafi Mahafid <rafimahafid@gmail.com>
2020-09-29 17:43:59 +0600

    change

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,8 +5,6 @@ function add(num1, num2) {
     const result = num1 + num2;
     alert('Result is ' + result);
 }
-add(4, 7);
-add(5, 5);
 
 recentNumber = (recentNumber + 15) * 4;
 let numberString = '(' + defaultValue + ' + 15) * 4';
