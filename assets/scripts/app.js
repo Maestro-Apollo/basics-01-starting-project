5e3396f2d8ed7acf8bfccc4361a3f46b9078df4f
Rafi Mahafid <rafimahafid@gmail.com>
2020-10-10 20:05:16 +0600

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,44 @@
 const defaultValue = 0;
 let recentNumber = defaultValue;
 
+function getEnterNumber() {
+    return parseInt(userInput.value);
+}
+
+function readAndWrite(operator, beforeOutput, finalResult) {
+    const resultDescription = `${beforeOutput} ${operator} ${finalResult}`;
+    outputResult(recentNumber, resultDescription);
+}
+
 function add() {
-    recentNumber = recentNumber + userInput.value;
-    outputResult(recentNumber, '');
+    const enterNumber = getEnterNumber();
+    const initNumber = recentNumber;
+    recentNumber = recentNumber + enterNumber;
+    readAndWrite('+', initNumber, enterNumber);
+}
+
+function sub() {
+    const enterNumber = getEnterNumber();
+    const initNumber = recentNumber;
+    recentNumber = recentNumber - enterNumber;
+    readAndWrite('-', initNumber, enterNumber);
+}
+
+function multi() {
+    const enterNumber = getEnterNumber();
+    const initNumber = recentNumber;
+    recentNumber = recentNumber * enterNumber;
+    readAndWrite('*', initNumber, enterNumber);
+}
+
+function divide() {
+    const enterNumber = getEnterNumber();
+    const initNumber = recentNumber;
+    recentNumber = recentNumber / enterNumber;
+    readAndWrite('/', initNumber, enterNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', sub);
+multiplyBtn.addEventListener('click', multi);
+divideBtn.addEventListener('click', divide);
